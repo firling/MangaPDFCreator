994ca3c2ee593b9b7a535f97075fc26b3da2437e
Julien Anquetil <anquetil.jul@gmail.com>
2020-05-12 23:10:53 +0200

    new site to look for manga on added + fix page build wasnt sorted well

--- script.py
+++ script.py
@@ -14,50 +14,70 @@ def extention(urlImage, name, chapter, page):
     return False
 
 
-name = "tokyo revengers"
-chapter = 2
+name = "dr stone"
+chapters = 21
 
-urlStarts = ["https://www.scan-vf.net/{}/chapitre-{}",
-             "https://www.lelscan-vf.com/manga/{}/{}",
-             "https://www.lelscan-vf.com/manga/{}/{}"]
-urlImagess = ["https://www.scan-vf.net/uploads/manga/{}/chapters/chapitre-{}/{:0>2d}.",
-              "https://www.lelscan-vf.com/uploads/manga/{}/chapters/{}vf/{:0>3d}.",
-              "https://www.lelscan-vf.com/uploads/manga/{}/chapters/{}/{:0>2d}."]
+for chapter in range(1, chapters+1):
 
-for i in range(len(urlStarts)):
-    urlStart = urlStarts[i]
-    urlImages = urlImagess[i]
-    ext = extention(urlImages, name, chapter, 1)
+    urlStarts = ["https://www.scan-vf.net/{}/chapitre-{}",
+                 "https://www.lelscan-vf.com/manga/{}/{}",
+                 "https://www.lelscan-vf.com/manga/{}/{}"]
+    urlImagess = ["https://www.scan-vf.net/uploads/manga/{}/chapters/chapitre-{}/{:0>2d}.",
+                  "https://www.lelscan-vf.com/uploads/manga/{}/chapters/{}vf/{:0>3d}.",
+                  "https://www.lelscan-vf.com/uploads/manga/{}/chapters/{}/{:0>2d}."]
+
+    for i in range(len(urlStarts)):
+        urlStart = urlStarts[i]
+        urlImages = urlImagess[i]
+        ext = extention(urlImages, name, chapter, 1)
+        if not ext:
+            continue
+        else:
+            urlImages = urlImages + extention(urlImages, name, chapter, 1)
+            break
+
     if not ext:
-        continue
-    else:
-        urlImages = urlImages + extention(urlImages, name, chapter, 1)
-        break
+        urlStart = "https://scan-fr.co/manga/{}/{}"
 
-if not ext:
-    exit()
+    res = requests.get(urlStart.format(name.replace(" ", "-").lower(), chapter), stream=True)
+    soup = BeautifulSoup(res.text, "lxml")
 
-# TODO  :  http://scan-fr.co/manga/tower-of-god/1/1
+    if not ext:
+        url = soup.find("img", {"class": "scan-page"})["src"].replace(" ", "")
+        chap = url.find(str(chapter))
+        page = url.rfind("1")
+        pageSlash = url.rfind("/")
+        pagePart = url[pageSlash - len(url) + 1:]
+        pagePartNew = pagePart[:pagePart.find(".")]
+        chapPart = url[:pageSlash - len(url)]
+        chapSlash = chapPart.rfind("/")
+        chapPart = chapPart[chapSlash - len(chapPart) + 1:]
+        urlImages = url.replace(pagePart, "{}" + pagePart[pagePart.find("."):]).replace(chapPart, "{}")
 
-res = requests.get(urlStart.format(name.replace(" ", "-").lower(), chapter), stream=True)
+    select = soup.find("select", {'id': "page-list"})
+    for i in range(len(select.findAll("option"))):
+        url = urlImages
+        if not ext:
+            res = requests.get(urlImages.format(chapPart, pagePartNew))
+            pagePartOld = pagePartNew
+            pagePartNew = pagePartNew.replace(str(i+1), str(i+2))
+            while len(pagePartNew) > len(pagePartOld):
+                pagePartNew = pagePartNew[1:]
+        else:
+            res = requests.get(url.format(name.replace(" ", "-").lower(), chapter, i+1), stream=True)
+        if res.status_code != 200:
+            continue
 
-soup = BeautifulSoup(res.text, "lxml")
-select = soup.find("select", {'id': "page-list"})
-for i in range(len(select.findAll("option"))):
-    url = urlImages
-    res = requests.get(url.format(name.replace(" ", "-").lower(), chapter, i+1), stream=True)
-    if res.status_code != 200:
-        continue
+        hex = codecs.encode(res.content, "hex").decode("ascii")
+        b64 = codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
 
-    hex = codecs.encode(res.content, "hex").decode("ascii")
-    b64 = codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
+        before = "0" if len(str(i+1)) == 1 else ""
+        with open("./img/img{}.png".format(before + str(i+1)), "wb") as f:
+            f.write(base64.decodebytes(bytes(b64, 'utf-8')))
+            f.close()
 
-    with open("./img/img{}.png".format(i+1), "wb") as f:
-        f.write(base64.decodebytes(bytes(b64, 'utf-8')))
-        f.close()
+    with open(name.replace(" ", "-") + str(chapter) + ".pdf", "wb") as fh:
+        fh.write(img2pdf.convert(["./img/" + i for i in os.listdir('./img')]))
 
-with open(name.replace(" ", "-") + str(chapter) + ".pdf", "wb") as fh:
-    fh.write(img2pdf.convert(["./img/" + i for i in os.listdir('./img')]))
-
-for i in os.listdir('./img'):
-    os.remove("./img/" + i)
+    for i in os.listdir('./img'):
+        os.remove("./img/" + i)
